91d8fcef8a14e45b87ee064f491f53b7dfc51181
Eva <yi.zheng1709@gmail.com>
2021-01-12 17:09:53 -0500

    commit tree 89cdb25660635d3add97e294347fa4039b827c7d

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(array)
+    new_array = []
+    array.each do |name|
+        new_array << "Hello, my name is #{name}."
+    end 
+    new_array 
+end 
+
+def assign_rooms(array)
+    room = 1
+    new_array = []
+    array.each do |name|
+        new_array << "Hello, #{name}! You'll be assigned to room #{room}!"
+        room += 1
+    end 
+    new_array 
+end 
+
+def printer (array)
+    (batch_badge_creator(array)).each do |name|
+        puts name 
+    end 
+    (assign_rooms(array)).each do |name|
+        puts name
+    end 
+end 
